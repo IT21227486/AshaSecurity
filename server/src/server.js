1e4ba0ac6bb182def30db9bb5e7f61ef749e96bf
IT21227486 <it21227486@my.sliit.lk>
2026-02-05 13:40:22 +0530

    Fix Render port

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,23 @@
+// import http from "http";
+// import app from "./app.js";
+// import { env } from "./utils/env.js";
+// import { connectMongo } from "./utils/mongo.js";
+
+// const server = http.createServer(app);
+
+// async function main() {
+//   await connectMongo();
+//   server.listen(env.PORT, () => {
+//     console.log(`Server listening on http://localhost:${env.PORT}`);
+//   });
+// }
+
+// main().catch((err) => {
+//   console.error("Fatal:", err);
+//   process.exit(1);
+// });
+
+
 import http from "http";
 import app from "./app.js";
 import { env } from "./utils/env.js";
@@ -7,8 +27,12 @@ const server = http.createServer(app);
 
 async function main() {
   await connectMongo();
-  server.listen(env.PORT, () => {
-    console.log(`Server listening on http://localhost:${env.PORT}`);
+
+  // ✅ Render provides PORT dynamically (process.env.PORT)
+  const port = Number(process.env.PORT || env.PORT || 5000);
+
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
   });
 }
 
